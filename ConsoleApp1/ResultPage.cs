f1400e9d1bca2706c9dd529b08c7bb9fea6039cd
Damon Kwok <damon-kwok@outlook.com>
2022-09-11 06:35:03 +0800

    Better Render

--- ConsoleApp1/ResultPage.cs
+++ ConsoleApp1/ResultPage.cs
@@ -4,27 +4,45 @@
 
 public static class ResultPage
 {
-    public static string Render(string title, List<CustomerInfo> results)
+    public static string Render(string title, List<CustomerInfo> results,
+        long rankBegin)
     {
-        return RenderHighlight(title, results, "");
-    }
-
-    public static string RenderEmpty(string title)
-    {
-        return RenderHighlight(title, new List<CustomerInfo>(), "");
-    }
-
-    public static string RenderHighlight(string title, List<CustomerInfo> results, string highlight)
-    {
-        string result = $"<h2>{title}</h2> <table border=\"1\">";
-        result += $"<tr> <th>Customer ID</th> <th>Score</th> <th>Rank</th> </tr>";
+        var result = $"<h2>{title}</h2> <table border=\"1\">";
+        result +=
+            $"<tr> <th>Customer ID</th> <th>Score</th> <th>Rank</th> </tr>";
 
         var i = 0;
         foreach (var info in results)
         {
-            var t1 = info!.Id == highlight ? "<td bgcolor=\"red\">" : "<td>";
-            var t2 = "</td>";
-            result += $"<tr> {t1}{info.Id}{t2} {t1}{info.Score}{t2}  {t1}{++i}{t2}</tr>";
+            var rank = rankBegin + i++;
+            var t1 = i % 2 == 1 ? "<td bgcolor=\"grey\">" : "<td>";
+            const string t2 = "</td>";
+            result +=
+                $"<tr> {t1}{info.Id}{t2} {t1}{info.Score}{t2}  {t1}{rank}{t2}</tr>";
+        }
+
+        result += "</table>";
+        return result;
+    }
+
+    public static string RenderEmpty(string title)
+    {
+        return Render(title, new List<CustomerInfo>(), 0);
+    }
+
+    public static string RenderHighlight(string title,
+        List<Tuple<long, CustomerInfo>> results, string highlight)
+    {
+        var result = $"<h2>{title}</h2> <table border=\"1\">";
+        result +=
+            $"<tr> <th>Customer ID</th> <th>Score</th> <th>Rank</th> </tr>";
+
+        foreach (var (rank, info) in results)
+        {
+            var t1 = info.Id == highlight ? "<td bgcolor=\"red\">" : "<td>";
+            const string t2 = "</td>";
+            result +=
+                $"<tr> {t1}{info.Id}{t2} {t1}{info.Score}{t2}  {t1}{rank}{t2}</tr>";
         }
 
         result += "</table>";
@@ -33,7 +51,7 @@ public static class ResultPage
 
     public static string Render404()
     {
-        var str = "404:\n\n Hello,Jerry\n Are you lost?";
+        const string str = "404:\n Are you lost?";
         return str;
     }
 }
